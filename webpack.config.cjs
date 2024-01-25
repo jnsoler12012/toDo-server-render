186ff84c008abc7dd131de8ab7e4b520e6f71d35
Nicolas Soler <jnsoler04@gmail.com>
2024-01-25 06:12:42 -0500

    render

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -14,7 +14,6 @@ module.exports = (env) => {
             path: path.resolve(__dirname, 'build/'),
             filename: 'bundle.cjs'
         },
-
         module: {
             rules: [
                 {
